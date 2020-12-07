cf6cc2cc199420c75050325658a7c0b88f45ab5f
Goblin-hub <56274669+Goblin-hub@users.noreply.github.com>
2020-12-07 13:09:36 +0100

    comment autocomplete.js

--- app/static/js/autocomplete.js
+++ app/static/js/autocomplete.js
@@ -1,3 +1,10 @@
+/**
+ * Main autocomplete function.
+ * Display some possible values for AutoComplete, based on already written value.
+ * @param {String} id_input, HMTL id of the input containing the value to autocomplete. 
+ * @param {String} id_autocomplete, HTML id of the div containing the autocomplete proposals.
+ * @param {*} data, Dictionary containing all possible values as keys.
+ */
 function checkvalues(id_input, id_autocomplete, data) {
     let input_element = document.getElementById(id_input);
     let autocomplete_element = document.getElementById(id_autocomplete);
@@ -23,12 +30,24 @@ function checkvalues(id_input, id_autocomplete, data) {
     }
 }
 
+/**
+ * Complete the passed input element.
+ * @param {String} text, Completion value. 
+ * @param {*} input_element, HTML input, Its value is replaced by text. 
+ * @param {*} autocomplete_element, HTML div where the proposals are displayed. 
+ */
 function complete(text, input_element, autocomplete_element) {
     input_element.value = text; 
     autocomplete_element.innerHTML = null;
     autocomplete_element.className ="dropdown-menu";
 }
 
+/**
+ * Add a new AutoComplete proposal.
+ * @param {*} text, Completion value
+ * @param {*} input_element, HTML input. 
+ * @param {*} autocomplete_element,  HTML div where the proposals are displayed. 
+ */
 function addAutoCompleteLine(text, input_element, autocomplete_element) {
     let textlink = document.createElement('a');
     textlink.href = '#';
